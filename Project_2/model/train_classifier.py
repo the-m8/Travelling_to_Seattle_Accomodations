8d3761219e1b39e6e0e1fd0aaf62ea1694ed9aef
Mona <84857216+the-m8@users.noreply.github.com>
2021-09-23 00:11:18 +0200

    Add files via upload

--- Project_2/model/train_classifier.py
+++ Project_2/model/train_classifier.py
@@ -1,24 +1,131 @@
 import sys
+import pandas as pd
+import numpy as np
+import sqlite3
+import pickle
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
+from sqlalchemy import create_engine
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.utils.multiclass import type_of_target
+from sklearn.ensemble import RandomForestClassifier
 
 def load_data(database_filepath):
-    pass
+    
+    """
+    Loading transformed data in database_filepath and split into X and y.
+    This function connects to the sqlite server DisasterResponseData.db, loads the stored dataset, 
+    removes the column "id" and splits the remaining columns into messages (X) and number values (y).
+   
+    Parameters:
+    None
+  
+    Returns:
+    array (X, Y): messages and number values.
+  
+    """
+    
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql('SELECT * FROM messages', engine)
+    df.drop(columns=['id'], inplace=True)
+    X = df.message.values
+    Y = df.select_dtypes(include=np.number).values
+    category_names = np.unique(Y)
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    
+    """
+    Transforming string text into arrays of words.
+    This function splits strings into words, removes stopwords and lemmatize the words.
+      
+    Parameters:
+    text (string): string input containing text.
+  
+    Returns:
+    lemmed (array): array with separate text strings for machine learning model input.
+  
+    """
+    tokens = word_tokenize(text.lower().strip())
+    tokens = [w for w in tokens if w not in stopwords.words("english")]
+    lemmed = [WordNetLemmatizer().lemmatize(w) for w in tokens]
+    return lemmed
 
 
 def build_model():
-    pass
+    
+    """
+    Building a prediction model.
+    This function contains a pipeline that vectorizes and categorizes the model input to predict y values.
+      
+    Parameters:
+    None
+  
+    Returns:
+    Model Pipeline.
+  
+    """
+    pipeline = Pipeline([
+            ('vec', CountVectorizer(tokenizer=tokenize)),
+            ('tfidf', TfidfTransformer()),
+            ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ])
+    
+   
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    
+    """
+    Evaluate and display model results.
+  
+    This function predicts the values and matches them against the actual values. 
+    It displays the f1 score, precision and recall in the classification report of the predicted output and shows how well the selected prediction model works.
+      
+    Parameters:
+    model: model output from build_model
+    X_test, Y_test: Test-split returned from build_model
+    category_names: Unique values of Y.
+  
+    Returns:
+    classification report: shows precision, recall, f1-score and support on different target names.
+      
+    """
+    Y_pred = model.predict(X_test)
+    type_of_target(Y_test)
+    type_of_target(Y_pred)
+    report = classification_report(np.hstack(Y_test),np.hstack(Y_pred))
+    print('Target names:', category_names)
+    print('Classification report:\n', report)
 
 
 def save_model(model, model_filepath):
-    pass
+    
+    """
+    Saving model to filepath.
+    This function saves a model to a chosen filepath.
+      
+    Parameters:
+    model: model to be saved.
+    model_filepath: path where model should be saved.
+  
+    Returns:
+    None
+  
+    """
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
